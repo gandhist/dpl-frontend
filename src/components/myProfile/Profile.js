11cff5e8753a70e5077464091314f0cbfee68fe4
gandhi <gandhisaputra13@gmail.com>
2020-12-29 22:31:47 +0700

    update password

--- src/components/myProfile/Profile.js
+++ src/components/myProfile/Profile.js
@@ -6,8 +6,11 @@ const Profile = () => {
     const access_token = JSON.parse(localStorage.getItem('access_token')).token;
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
     const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
 
+    // get user detals
     const getUser = async () =>{
         try {
             await fetch('http://127.0.0.1:8000/api/user', {
@@ -27,6 +30,7 @@ const Profile = () => {
                             // console.log("result", res)
                             setUser(res.data);
                             setEmail(res.data.email);
+                            setPhone(res.data.phone ? res.data.phone : '');
                             setName(res.data.name);
                         })
                     }
@@ -35,6 +39,48 @@ const Profile = () => {
             
         } catch {
             alert("something went wrong")
+        }
+    }
+
+    // handle update data
+    const updateUser = async () => {
+        try {
+            setLoading(true)
+            let dataUpdate = {
+                name : name,
+                phone : phone,
+                password : password
+            }
+            await fetch('http://127.0.0.1:8000/api/user',{
+                method: "POST",
+                headers : {
+                    'Accept' : 'application/json',
+                    'Content-Type' : 'application/json',
+                    'Authorization' : `Bearer ${access_token}`
+                },
+                body : JSON.stringify(dataUpdate)
+            }).then(
+                (res) => {
+                    if(res.status === 500){
+                        alert('something went wrong')
+                    }
+                    else if(res.status == 422){
+                        alert(res.message)
+                    }
+                    else {
+                        res.json().then((res) => {
+                            console.warn('update', res)
+                            getUser();
+                            alert(res.meta.message)
+                        })
+                    }
+                }
+            );
+        } catch (e) {
+            setLoading(false)
+            console.log(e.message)
+        } finally {
+            setLoading(false)
         }
     }
 
@@ -105,7 +151,7 @@ const Profile = () => {
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z" />
                             </svg>
                         </div>
-                        <input type="text" className="w-11/12 focus:outline-none focus:text-gray-600 p-2" placeholder={12341234} />
+                        <input type="text" className="w-11/12 focus:outline-none focus:text-gray-600 p-2" value={phone} onChange={e => setPhone(e.target.value)} placeholder={12341234} />
                         </div>
                     </div>
                     </div>
@@ -124,15 +170,17 @@ const Profile = () => {
                     </div>
                     </div>
                     <div className="md:w-3/12 text-center md:pl-6">
-                    <button className="text-white w-full mx-auto max-w-sm rounded-md text-center bg-indigo-400 py-2 px-4 inline-flex items-center focus:outline-none md:float-right">
+                    <button type="button" onClick={updateUser} className="text-white w-full mx-auto max-w-sm rounded-md text-center bg-indigo-400 py-2 px-4 inline-flex items-center focus:outline-none md:float-right">
                         <svg fill="none" className="w-4 text-white mr-2" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
                         </svg>
-                        Update
+                        {
+                            loading ? 'Loading...' : 'Update'
+                        }
                     </button>
                     </div>
                 </div>
-                <hr />
+                {/* <hr />
                 <div className="w-full p-4 text-right text-gray-500">
                     <button className="inline-flex items-center focus:outline-none mr-4">
                     <svg fill="none" className="w-4 mr-2" viewBox="0 0 24 24" stroke="currentColor">
@@ -140,7 +188,7 @@ const Profile = () => {
                     </svg>
                     Delete account
                     </button>
-                </div>
+                </div> */}
                 </div>
             </div>
 
